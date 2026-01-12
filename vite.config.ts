c2a933d813c0ff10322f98d497fee143aa5081be
yuri <yu_ri0411@naver.com>
2026-01-12 15:16:19 +0900

    chore: alias 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      // "@apis": path.resolve(__dirname, "src/apis"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+});
