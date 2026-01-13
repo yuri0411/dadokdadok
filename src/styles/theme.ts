7c68cd0a3789b30d38240f37b3b254155a332832
yuri <yu_ri0411@naver.com>
2026-01-13 14:17:58 +0900

    feat: style 관련 파일 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+export const colors = {
+  primary: "#00B894",
+  white: "#ffffff",
+  border: "#dee2e6",
+  text: {
+    primary: "#333333",
+    secondary: "#666666",
+    tertiary: "#868e96",
+    disabled: "#999999",
+  },
+};
+
+export const sizes = {
+  sm: "31px",
+  md: "34px",
+  lg: "48px",
+  xl: "52px",
+} as const;
+
+export const typography = {
+  h4: "typography-h4",
+  h5: "typography-h5",
+  h6: "typography-h6",
+  body: "typography-body",
+  caption: "typography-caption",
+  overline: "typography-overline",
+};
