785945b3a48506b3e96cac5119e1d381e90e7316
yuri <yu_ri0411@naver.com>
2026-01-14 23:16:07 +0900

    feat: formatTime 유틸 함수 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,3 +11,15 @@ export function cls(...args: any[]) {
     )
     .join(" ");
 }
+
+export function formatTime(seconds: number) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor(seconds / 60) % 60;
+  const secs = seconds % 60;
+
+  const result = [hours > 0 && `${hours}시간`, minutes > 0 && `${minutes}분`, `${secs}초`]
+    .filter(Boolean)
+    .join(" ");
+
+  return seconds === 0 ? `0초` : result;
+}
