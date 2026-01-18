6785f9033ca780d97b0f7d1c1510ff78fe6254b6
yuri <yu_ri0411@naver.com>
2026-01-19 00:34:21 +0900

    fix: wrapper에 내용 추가 및 스타일 수정

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,4 +1,7 @@
 import { Outlet } from "react-router-dom";
+import { Stack, Typography } from "@/components";
+import { FaGithub } from "react-icons/fa";
+import { LuExternalLink } from "react-icons/lu";
 
 const RootLayout = () => {
   return (
@@ -8,10 +11,67 @@ const RootLayout = () => {
         width: "100vw",
         height: "100vh",
         justifyContent: "center",
+        backgroundColor: "#00B894",
       }}
     >
-      <div style={{ width: "43rem", padding: 20 }}>wrapper</div>
-      <div style={{ width: "43rem", height: "100%", overflowY: "scroll", background: "white" }}>
+      <Stack justify="center" gap={30} style={{ width: "30rem", padding: 20, color: "white" }}>
+        <Stack>
+          <Typography as="h1" variant="h2" color="inherit">
+            다독다독
+          </Typography>
+          <Typography as="h4" variant="h4" color="inherit">
+            JLPT 단어, 반복 회독으로 자연스럽게 학습하자!
+          </Typography>
+        </Stack>
+        <Stack>
+          <Typography as="p" variant="body" color="inherit">
+            한 번 보면 금방 잊어버리던 단어들, 다독다독은 반복 회독을 통해 단어가 스며들듯
+            익혀지도록 도와주는 JLPT 단어 학습 서비스입니다.
+          </Typography>
+          <br />
+          <Typography as="p" variant="body" color="inherit">
+            헷갈리는 단어만 모아주는 효율적인 복습 구조와 누적 기록으로 확인할 수 있는 성장 흐름이
+            당신의 꾸준함을 가볍지만 오래도록 이어줍니다.
+          </Typography>
+        </Stack>
+        <Stack direction="horizontal" gap={8}>
+          <Typography
+            as="a"
+            target="_blank"
+            href="https://yuri-archive.notion.site/2b13058aa1588089b52fe169bf0c3969"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: "4px",
+              background: "white",
+              padding: "12px 16px",
+              borderRadius: "50px",
+            }}
+          >
+            <LuExternalLink />
+            개발 과정 자세히 보기
+          </Typography>
+          <Typography
+            as="a"
+            variant="body"
+            target="_blank"
+            href="https://github.com/yuri0411/dadokdadok"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: "4px",
+              background: "white",
+              padding: "8px 16px",
+              borderRadius: "50px",
+            }}
+          >
+            <FaGithub /> github 바로가기
+          </Typography>
+        </Stack>
+      </Stack>
+      <div style={{ width: "30rem", height: "100%", overflowY: "scroll", background: "white" }}>
         <div id="content-root" style={{ position: "relative", height: "100%" }}>
           <Outlet />
         </div>
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,13 +2,29 @@ import RootLayout from "../layouts/RootLayout.tsx";
 import { createBrowserRouter, Navigate, type RouteObject } from "react-router-dom";
 import { PATHS } from "./paths.ts";
 import { lazy, type ReactNode, Suspense } from "react";
+import { Stack } from "@/components";
 
 const Home = lazy(() => import("../pages/Home"));
 const Unit = lazy(() => import("../pages/Unit"));
 const Word = lazy(() => import("../pages/Word"));
 
 const Lazy = ({ children }: { children: ReactNode }) => (
-  <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+  <Suspense
+    fallback={
+      <Stack
+        align="center"
+        justify="center"
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        Loading...
+      </Stack>
+    }
+  >
+    {children}
+  </Suspense>
 );
 
 const routes: RouteObject[] = [
